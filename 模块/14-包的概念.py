5d9fed842fa00df84190d62c2124b9e647912589
weizt <weizhetao3@163.com>
2020-09-16 17:09:17 +0800

    面向对象的学习

---
+++ 模块/14-包的概念.py
@@ -0,0 +1,9 @@
+# 概念：多个相似或有关联的模块打包至一个文件夹叫包
+# pyhton包里，会有一个__init__.py文件
+# 调用包的方法
+'''
+from <directory_name> import  <module_name>
+from <directory_name.module.name> import <variable_name>
+'''
+
+x = '我是send_msg里的变量'
---
+++ 面向对象/02-面向对象的基本语法.py
@@ -0,0 +1,40 @@
+# 需求1
+'''
+小明今年18岁，身高1.75米，每天早上跑完步会去吃东西
+小美今年17岁，身高1.65，小美每天不跑步，小美喜欢吃东西
+'''
+# 定义类:
+'''
+类名定义要遵守大驼峰命名法
+ 1.class <类名>:
+ 2.class <类名>(object):
+
+'''
+
+
+class Student(object):  # 关注这个类有哪些特征和方法
+    # 在__init__方法里，以参数的形式定义特征，我们称之为属性
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+
+    # 行为定义为一个个函数
+    def run(self):
+        print('正在跑步')
+
+    def eat(self):
+        print('正在吃东西')
+
+
+# Student() ==> 会自动调用__init__方法
+# 使用了Student类创建了两个实例对象s1、s2
+# s1和s2都会有三个属性，同时都有run和eat方法
+s1 = Student('小明', 18, 1.75)
+s2 = Student('小美', 17, 1.65)
+
+# 根据业务逻辑，让不同的对象执行不同的行为
+s1.run()
+s1.eat()
+
+s2.eat()
